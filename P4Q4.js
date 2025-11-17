0471b2643ef17bfe93106d641942f7c13f6f32d5
Jeff Teoh <jeff.teoh.01@gmail.com>
2025-11-17 16:30:18 +1100

    Q4.2 Create array of movies - unsorted

--- P4Q4.js
+++ P4Q4.js
@@ -8,3 +8,19 @@ class Movie{
     }
 }
 console.log(Movie);
+
+//  Question 4.2 Create array of movies - unsorted
+let movies = [
+    new Movie(37, "The Matrix", 1999, 8.7),
+    new Movie(12, "Inception", 2010, 8.8),
+    new Movie(90, "Interstellar", 2014, 8.6),
+    new Movie(5, "Jurassic Park", 1993, 8.1),
+    new Movie(63, "Avatar", 2009, 7.8),
+    new Movie(28, "Titanic", 1997, 7.9),
+    new Movie(44, "Iron Man", 2008, 7.9),
+    new Movie(81, "The Dark Knight", 2008, 9.0),
+    new Movie(19, "Gladiator", 2000, 8.5),
+    new Movie(56, "Braveheart", 1995, 8.3)
+];
+
+console.log(movies);
