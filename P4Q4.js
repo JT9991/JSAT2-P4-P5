2066d9289f06334af3df4fc96ccaa18abfe1ccf0
Jeff Teoh <jeff.teoh.01@gmail.com>
2025-11-17 16:13:07 +1100

    Q4.1 build the class

---
+++ P4Q4.js
@@ -0,0 +1,9 @@
+// Question 4.1 Create a movie class
+class Movie{
+    constructor(id, title, year, rating){
+        this.id = id;
+        this.title = title;
+        this.year=year;
+        this.rating=rating;
+    }
+}
